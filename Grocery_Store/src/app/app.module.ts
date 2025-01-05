af5787609ae3785793e58c82c9251fa6bcead8de
Aaron Mubiruan <mubiruaaron2003@gmail.com>
2025-01-05 18:27:39 -0400

    Fixed Project Initialization with html mark up

--- Grocery_Store/src/app/app.module.ts
+++ Grocery_Store/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
