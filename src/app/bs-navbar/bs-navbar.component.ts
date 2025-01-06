d631b585f03224d77aaaa23d7de65aea646f5738
Aaron Mubiruan <mubiruaaron2003@gmail.com>
2025-01-06 18:11:41 -0400

    Initial Simple implementation of logOut Button with tracking the authState then writing out to the console

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrl: './bs-navbar.component.css',
 })
-export class BsNavbarComponent {}
+export class BsNavbarComponent {
+  constructor(private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe((x) => console.log(x));
+  }
+
+  logout() {
+    this.afAuth.signOut();
+  }
+}
