d25e30b1846cc3e2d17fb5d0aad50827cebe7b48
Aaron Mubiruan <mubiruaaron2003@gmail.com>
2025-01-09 22:04:08 -0400

    Implemented initial Displaying quantity

--- src/app/products/product-card/product-card.component.ts
+++ src/app/products/product-card/product-card.component.ts
@@ -10,12 +10,20 @@ import { ShoppingCartService } from '../../services/shopping-cart.service';
 export class ProductCardComponent {
   @Input('product') product!: Product;
   @Input('show-actions') showActions = true;
+  @Input('shopping-cart') shoppingCart: any = {};
   constructor(private cartService: ShoppingCartService) {}
 
   addToCart(product: Product) {
     //first we get the cart id from the local storage
     this.cartService.addToCart(product);
-
-   
   }
+
+
+  getQuantity() {
+    if (!this.shoppingCart) return 0;
+    const item = this.shoppingCart.items[this.product.$key];
+    return item ? item.quantity : 0;
+  }
+
+
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
+import { ShoppingCartService } from '../services/shopping-cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit , OnDestroy{
   products: Product[] = [];
   filteredProducts: Product[] = [];
-  
+  cart: any;
   category: string | null = null;
+  subscription!: Subscription;
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
+    private shoppingCartService: ShoppingCartService
   ) {
+
+
+
     productService
       .getAll()
       .subscribe((products) => {
@@ -33,6 +40,17 @@ export class ProductsComponent {
     
 
   
+  }
+
+
+
+ async ngOnInit() {
+  this.subscription = (await this.shoppingCartService.getCart()).valueChanges().subscribe((cart) => {
+    this.cart = cart;
+  });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
 
